5213584b76fd3720530e7b857f36e41e0141ace5
Hifumi-GGS <reignjoshua.parnaso@unc.edu.ph>
2024-08-14 23:44:11 +0800

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,9 +1,9 @@
     function sendEmailYes() {
        let parms = {
-      name : Notif.me
-      email : parnasoreignjoshua@gmail.com
-    subject : notif: 
-    message : GET READY, SHE SAID YES!!!
+      name : "Notif.me"
+      email : "parnasoreignjoshua@gmail.com"
+    subject : "notif: "
+    message : "GET READY, SHE SAID YES!!!"
    }
   emailjs.send(service_u8gefiz, template_h43bagj, parms)
      }
