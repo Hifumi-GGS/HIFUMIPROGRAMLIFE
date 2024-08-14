99df68fbec775a6a8022c20c16e3c067d697162d
Hifumi-GGS <reignjoshua.parnaso@unc.edu.ph>
2024-08-14 15:34:14 +0800

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,53 @@
+const yesBtn = document.getElementById('yes-btn');
+    const noBtn = document.getElementById('no-btn');
+   
+    yesBtn.addEventListener('click', () => {
+      alert('Reign is happy! See you soon!');
+      sendEmailYes();
+    });
+    noBtn.addEventListener('click', () => {
+      alert('Reign is sad');
+      sendEmailNo();
+    });
+
+  
+
+    function sendEmailNo() {
+        Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "parnasoreignjoshua@gmail.com",
+    Password : "157FF7E4DA37CA98CCB3DE8ED382DF188317",
+    To : 'reignjoshua.parnaso@unc.edu.ph',
+    From : "parnasoreignjoshua@gmail.com",
+    Subject : "Her Response",
+    Body : "MAN DOWN! I REPEAT MAN DOWN!!"
+}).then(
+  message => alert(Sent to Reign!!)
+);
+    }
+    
+    function sendEmailYes() {
+        Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "parnasoreignjoshua@gmail.com",
+    Password : "157FF7E4DA37CA98CCB3DE8ED382DF188317",
+    To : 'reignjoshua.parnaso@unc.edu.ph',
+    From : "parnasoreignjoshua@gmail.com",
+    Subject : "Her Response",
+    Body : "DRESS UP, SHE SAID YES!!!"
+}).then(
+  message => alert(Sent to Reign!!)
+);
+    }
+
+   </script>
+   <script>
+     const audio = document.getElementById('audio');
+    const playBtn = document.getElementById('play-btn');
+    const pauseBtn = document.getElementById('pause-btn');
+      playBtn.addEventListener('click', () => {
+      audio.play();
+    });
+    pauseBtn.addEventListener('click', () => {
+      audio.pause();
+    });
