01b8a55f701c26a0185013bf091ceefba80b7636
Hifumi-GGS <reignjoshua.parnaso@unc.edu.ph>
2024-08-14 15:35:41 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,18 +1,9 @@
-const yesBtn = document.getElementById('yes-btn');
-    const noBtn = document.getElementById('no-btn');
-    const audio = document.getElementById('audio');
+
+   const audio = document.getElementById('audio');
     const playBtn = document.getElementById('play-btn');
     const pauseBtn = document.getElementById('pause-btn');
 
-    yesBtn.addEventListener('click', () => {
-      alert('Reign is happy! See you soon!');
-      sendEmail('CHOP CHOP BOY, SHE SAID YES!!');
-    });
-    noBtn.addEventListener('click', () => {
-      alert('Reign is sad');
-      sendEmail('She said no, Too bad man...');
-    });
-
+    
     playBtn.addEventListener('click', () => {
       audio.play();
     });
@@ -20,9 +11,4 @@ const yesBtn = document.getElementById('yes-btn');
       audio.pause();
     });
 
-    function sendEmail(message) {
-      const email = 'reignjoshua.parnaso@unc.edu.ph'
-      const subject = 'Notif: ';
-      const body = `Subject: ${subject}\r\n\r\n${message}`;
-      window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
-    }
+    
