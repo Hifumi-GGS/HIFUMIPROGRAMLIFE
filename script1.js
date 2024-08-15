0102720a52a70a240349ea1b7f2eebe2314fa13f
Hifumi-GGS <reignjoshua.parnaso@unc.edu.ph>
2024-08-15 11:18:26 +0800

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,7 +3,15 @@
     const playBtn = document.getElementById('play-btn');
     const pauseBtn = document.getElementById('pause-btn');
 
-    
+     const yesBtn = document.getElementById('yes-btn');
+    const noBtn = document.getElementById('no-btn');
+   
+    yesBtn.addEventListener('click', () => {
+      Swal.fire("REIGN IS VERY HAPPY");
+    });
+    noBtn.addEventListener('click', () => {
+      Swal.fire("REIGN IS VERY SAD");
+    });
     playBtn.addEventListener('click', () => {
       audio.play();
     });
